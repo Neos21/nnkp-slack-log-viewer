4d57ba0082fbd209f3f7e6c599da2d79929f9771
Neos21 <neos21@gmail.com>
2024-04-26 18:56:08 +0900

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 
 import { UsersService } from './services/users.service';
 import { MenuComponent } from './components/menu/menu.component';
-import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    FormsModule,
     RouterOutlet,
     MenuComponent
   ],
@@ -16,6 +18,9 @@ import { DOCUMENT } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+  public isLoggedIn = false;
+  public inputPassword = '';
+  
   private isShownMenu = false;
   
   constructor(
@@ -26,12 +31,6 @@ export class AppComponent implements OnInit {
   ) { }
   
   public async ngOnInit(): Promise<void> {
-    try {
-      await this.usersService.fetchUsers();
-    }
-    catch(error) {
-      console.error('AppComponent#ngOnInit()', error);
-    }
     // ページ遷移時はサイドメニューを閉じ、ページトップに遷移させる
     this.router.events.subscribe(event => {
       if(event instanceof NavigationEnd) {
@@ -39,10 +38,31 @@ export class AppComponent implements OnInit {
         window.scrollTo(0, 0);
       }
     });
+    
+    // LocalStorage にパスワードが見つかったら自動的にログインする
+    const localStoragePassword = window.localStorage.getItem('password');
+    if(localStoragePassword) {
+      this.inputPassword = localStoragePassword;
+      this.onLogin();
+    }
   }
   
   public toggleMenu(isShown?: boolean): void {
     this.isShownMenu = isShown != null ? isShown : !this.isShownMenu;
     this.renderer2[this.isShownMenu ? 'addClass' : 'removeClass'](this.document.body, 'show-menu');
+  }
+  
+  public async onLogin(): Promise<void> {
+    this.isLoggedIn = window.btoa(this.inputPassword) === 'MjAyMzA0MTg=';  // `window.btoa()` で生成しておいたパスワードと突合する
+    if(!this.isLoggedIn) return window.alert('パスワードが違います');
+    window.localStorage.setItem('password', this.inputPassword);  // 入力したパスワードを控えておく
+    
+    // ログインできたらユーザ一覧を取得する・同様に `this.isLoggedIn` が `true` になることで各種コンポーネントが動作する
+    try {
+      await this.usersService.fetchUsers();
+    }
+    catch(error) {
+      console.error('AppComponent#onLogin()', error);
+    }
   }
 }
